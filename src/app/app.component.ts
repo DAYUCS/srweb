b844f291e98998cb215f7b945be92d37ca661883
Dayu Bian <dayub@chinasystems.com>
2023-09-24 10:39:23 +0800

    Web Speech Recognition

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+declare var webkitSpeechRecognition: any;
+declare var SpeechRecognition: any;
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'srweb';
+  recognizedText: string = '';
+  recording = false;
+  recognition: any;
+  
+  constructor() {
+    this.recognition = new webkitSpeechRecognition() || new SpeechRecognition();
+    this.recognition.continuous = true;
+    this.recognition.interimResults = true;
+    this.recognition.lang = 'en-US';
+
+    this.recognition.addEventListener('result', (event: any) => {
+      this.recognizedText = event.results[0][0].transcript;
+      console.log(`Confidence: ${event.results[0][0].confidence}`);
+    });
+  }
+
+  startRecording() {
+    this.recording = true;
+    this.recognizedText = '';
+    this.recognition.start();
+  }
+
+  stopRecording() {
+    this.recording = false;
+    this.recognition.stop();
+  }
+
 }
